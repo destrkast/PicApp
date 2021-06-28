664f6be352ced6446ba93f7827ddab31a52c9bc1
Konstantin Dvurechenskiy <destrkast29@gmail.com>
2021-06-28 11:25:40 +0300

    Creation of a sign up page

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,31 +4,30 @@ import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 import { set } from 'date-fns';
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
+  const [userName, setUserName] = useState('');
+  const [fullName, setFullName] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = async (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
 
     try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-      history.push(ROUTES.DASHBORAD);
+
     } catch (error) {
-      setEmailAddress('');
-      setPassword('');
-      setError(error.message);
+
     }
   };
 
   useEffect(() => {
-    document.title = 'Login Instagram';
+    document.title = 'Sign up Instagram';
   }, []);
 
   return (
@@ -49,13 +48,30 @@ export default function Login() {
             />
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignUp} method="POST">
+            <input
+              type="text"
+              aria-label="Enter your username"
+              placeholder="Username"
+              className="text=sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUserName(target.value)}
+              value={userName}
+            />
+            <input
+              type="text"
+              aria-label="Enter your full name"
+              placeholder="Full name"
+              className="text=sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
             <input
               type="text"
               aria-label="Enter your email"
               placeholder="Email address"
               className="text=sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress}
             />
             <input
               type="password"
@@ -63,6 +79,7 @@ export default function Login() {
               placeholder="Password"
               className="text=sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setPassword(target.value)}
+              value={password}
             />
             <button
               disabled={isInvalid}
@@ -70,15 +87,15 @@ export default function Login() {
               className={`bg-blue-medium text-white w-full rounded h-8 font-bold
             ${isInvalid && ' opacity-50'}`}
             >
-              Log In
+              Sign Up
             </button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
           <p className="text-sm">
-            Don't have an account?{` `}
-            <Link to="/signup" className="font-bold text-blue-medium">
-              Sign up
+            Have an account?{` `}
+            <Link to="/login" className="font-bold text-blue-medium">
+              Log In
             </Link>
           </p>
         </div>
