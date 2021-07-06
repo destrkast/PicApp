6ec7063e809fb2c0ab490b909c382af7f2cfc45d
Konstantin Dvurechenskiy <destrkast29@gmail.com>
2021-07-06 21:02:10 +0300

    Dealing with header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,64 @@
-import React from "react"
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
+import * as ROUTES from '../constants/routes';
 
 export default function Header() {
-  return(
-    <p>I am the Header</p>
-  )
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
+  return (
+    <header className="h-16 bg-white border-b border-gray-primary mb-8">
+      <div className="container mx-auto max-w-screen-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1 className="flex justify-center w-full">
+              <Link to={ROUTES.DASHBORAD} aria-label="Instagram logo">
+                <img
+                  src="/images/logo.png"
+                  alt="Instagram"
+                  className="mt-2 w-6/12"
+                />
+              </Link>
+            </h1>
+          </div>
+          <div className="text-gray-700 text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBORAD} aria-label="Dashboard">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                </Link>
+                <button
+                  type="button"
+                  title="Sign out"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                >Sign out</button>
+              </>
+            ) : (
+              <p>Heyoooo</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
