0e4d81907bc7cedbb5d0d6b1a85568a3a7ba3356
Konstantin Dvurechenskiy <destrkast29@gmail.com>
2021-08-12 17:34:10 +0300

    dealing with timeline

---
+++ src/components/post/index.js
@@ -0,0 +1,10 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Post({ content }) {
+  return <p>I am a post!</p>;
+}
+
+Post.propTypes = {
+
+}
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 //I need to get logged in user's photos (hook)
 //on loading photos I'm going to use  react skeleton
@@ -12,16 +13,16 @@ export default function Timeline() {
   console.log('photos', photos);
   return (
     <div className="container col-span-2">
-      {photos ? (
+      {!photos ? (
         <>
         {[...new Array(4)].map(
           (_, index) =>
-          <Skeleton key={index} count={4} width={320} height={600} className="mb-5"/>
+          <Skeleton key={index} count={4} width={640} height={500} className="mb-5"/>
         )}
         </>
       ) : photos?.length > 0 ? (
         photos.map((content) =>
-        <p key={content.docId}>{content.imageSrc}</p>)
+        <Post key={content.docId} content={content} />)
       ) : (
         <p className="text-center text-2xl">Follow people to see photos</p>
       )}
