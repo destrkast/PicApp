58bde0b0add9eb6a70b5ead3d705d20b7d883ce8
Konstantin Dvurechenskiy <destrkast29@gmail.com>
2021-08-16 17:57:44 +0300

    Added a post header and images

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,10 +1,27 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
+import Header from './header';
+import Image from './image';
 
 export default function Post({ content }) {
-  return <p>I am a post!</p>;
+  //header, image, action, footer, comments
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+    </div>
+  );
 }
 
 Post.propTypes = {
-
-}
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  }),
+};
